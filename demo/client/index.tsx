6bb435ecc7abe23016b9da87c8dd17704107c1f9
allisonIsCoding <allison-jacobs@outlook.com>
2021-05-13 18:27:24 -0400

    updated and fixed webpack. Various errors, including not specifying engine, unnecessary dependencies in the package.json, alongside some configuration needed.

---
+++ demo/client/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+// import * as ReactDOM from 'react-dom';
+// import { ThemeProvider } from '@material-ui/core/styles';
+// import App from './App';
+// import theme from './theme';
+
+// ReactDOM.render(
+//   <ThemeProvider theme={theme}>
+//     {/* <Documentation /> */}
+//     <App />
+//   </ThemeProvider>,
+//   document.getElementById('root'),
+// );
