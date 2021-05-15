b2499ae1c2113fea0cfaf7c5103ee4973bf98227
Jason Fricano <jasonfricano@Ninjas-MBP.local>
2021-05-15 17:02:46 -0400

    add subject class converted to ts

--- lib/Subject.ts
+++ lib/Subject.ts
@@ -1,33 +1,43 @@
 //TBD: bring the functionality of the Consumer class into Subject?
+const Consumer = require('./Consumer.ts');
+
+
+interface ioInterface {
+  on: Function;
+}
+
+interface ConsumerInterface {
+  run: Function;
+}
 
 class Subject {
-  io: object;
-  consumers: object[];
+  io: ioInterface
+  consumerArr: ConsumerInterface[];
 
   // io is socket server instance
-  // consumers is an array of KafkaSocks consumers (from the Consumer class we created)
-  constructor(io: object, consumers: object[] = []) {
+  // consumerArr is an array of KafkaSocks consumerArr (from the Consumer class we created)
+  constructor(io: ioInterface, consumerArr: ConsumerInterface[] = []) {
     this.io = io;
-    this.consumers = consumers;
+    this.consumerArr = consumerArr;
   }
 
-  // adds a KafkaSocks Consumer to the consumers array property
-  add(consumer: object) {
+  // adds a KafkaSocks Consumer to the consumerArr array property
+  add(consumer: ConsumerInterface) {
     // instantiate the Kakfa Consumer using the Kafa-provided class
     // add that consumer to our array
-    this.consumers.push(consumer);
+    this.consumerArr.push(consumer);
   }
 
   // opening the io server
-  // invoke the running of the sockets corresponding to the varoius consumers in our consumers array
+  // invoke the running of the sockets corresponding to the varoius consumerArr in our consumerArr array
   connect() {
-  //   this.io.on("connection", (socket: object) => {
-  //     // we need the socket to be listening to the event here
-  //     // there is no socket.on('this.event')
+    this.io.on("connection", (socket: object) => {
+      // we need the socket to be listening to the event here
+      // there is no socket.on('this.event')
 
-  //     this.consumers.forEach((consumer: object) => {
-  //       consumer.run(this.io);
-  //     });
-  //   });
+      this.consumerArr.forEach((consumer: ConsumerInterface) => {
+        consumer.run(this.io);
+      });
+    });
   }
 }
