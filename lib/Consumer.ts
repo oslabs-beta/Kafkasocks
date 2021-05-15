092ea5df96911679fc075ccda9eede46316b655b
Jason Fricano <jasonfricano@Ninjas-MBP.local>
2021-05-15 16:51:21 -0400

    add consumer class using ts WIP: refactor typing?

--- lib/Consumer.ts
+++ lib/Consumer.ts
@@ -1,16 +1,45 @@
 //to be translated into typescript
+// const { Kafka } = require('kafkajs');
+// const Subject = require('./Subject.ts');
 
 //what functionality can we add here that would differentiate this from what's provided by Kafka? 
 
+/* Note to Allison... Jason has chosen a path. This is the path we took. We ~may~ need to refactor these type declarations in the future*/
+
+// if this doesn't work use any as type of consumer
+// or have Allison (our Typescript guru) fix it
+// this might not work, but we wont know until Subject is built and we test
+interface ConsumerInterface {
+  connect: Function;
+  subscribe: Function;
+  run: Function;
+}
+
+interface ioInterface {
+  emit: Function;
+}
+
+interface EventInterface {
+  topic: string;
+  partition: string;
+  message: Message;
+}
+
+interface Message {
+  value: string;
+}
+
 export class Consumer {
-  consumer: object; //will we make these three an instance of a Kafka object or are we going to set an interface for each?
+  //consumer type is currently set to 'any' for functionality purpose.
+  //we would like to have a specific type declared so that connect, subscribe and any other key would be accepted. 
+  consumer: ConsumerInterface; //will we make these three an instance of a Kafka object or are we going to set an interface for each?
   topic: string;
   event: string;
 
   // consumer is a Kafka consumer object
   // topic is the Kafka object's topic
   // event is the socket.io event on which the kafka message is being passed
-  constructor(consumer: object, topic: string, event: string) {
+  constructor(consumer: ConsumerInterface, topic: string, event: string) {
     this.consumer = consumer; //
     this.topic = topic;
     this.event = event;
@@ -19,23 +48,23 @@ export class Consumer {
   
   // instantiate the Kafka consumer on the passed topic
   // and subscribes with that consumer
-  async run(io: object) {
-  //   await this.consumer.connect();
+  async run(io: ioInterface) {
+    await this.consumer.connect();
 
-  //   await this.consumer.subscribe({
-  //     topic: this.topic,
-  //     // topic: process.env.TOPIC,
-  //     fromBeginning: true,
-  //   });
+    await this.consumer.subscribe({
+      topic: this.topic,
+      // topic: process.env.TOPIC,
+      fromBeginning: true,
+    });
 
-  //   await this.consumer.run({
-  //     eachMessage: async ({ topic, partition, message }) => {
-        // io.emit(this.event, message.value.toString());
-  //       console.log(
-  //         "received Message from kafka",
-  //         JSON.parse(message.value.toString())
-  //       );
-  //     },
-  //   });
+    await this.consumer.run({
+      eachMessage: async (eventInfo: EventInterface) => {
+        io.emit(this.event, eventInfo.message.value.toString());
+        console.log(
+          "received Message from kafka",
+          JSON.parse(eventInfo.message.value.toString())
+        );
+      },
+    });
   }
 }
