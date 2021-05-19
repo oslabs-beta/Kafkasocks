4fc3bee84a9de46a667e2f89ecb1d90be7e483d9
Vinit Patel <vinit@Vinits-MacBook-Pro.local>
2021-05-19 17:05:44 -0400

    demo restart

--- kafka-socks/Subject.js
+++ kafka-socks/Subject.js
@@ -1,6 +1,12 @@
 "use strict";
-//TBD: bring the functionality of the Consumer class into Subject?
 exports.__esModule = true;
+// interface ioInterface {
+//   on: Function;
+// }
+// interface ConsumerInterface {
+//   run: Function;
+//   pause: Function;
+// }
 var Subject = /** @class */ (function () {
     // io is socket server instance
     // consumerArr is an array of KafkaSocks consumerArr (from the Consumer class we created)
@@ -12,24 +18,19 @@ var Subject = /** @class */ (function () {
     }
     // adds a KafkaSocks Consumer to the consumerArr array property
     Subject.prototype.add = function (consumer) {
-        // instantiate the Kakfa Consumer using the Kafa-provided class
-        // add that consumer to our array
+        // instantiate the Kakfa Consumer using the Kafa-provided class + add that consumer to our array
         this.consumerArr.push(consumer);
     };
     // opening the io server
     // invoke the running of the sockets corresponding to the varoius consumerArr in our consumerArr array
     Subject.prototype.connect = function () {
-        // this.io.on('connection', (socket: object) => {
-        //   // we need the socket to be listening to the event here
-        //   // there is no socket.on('this.event')
-        //   this.consumerArr.forEach((consumer: ConsumerInterface) => {
-        //   consumer.run(this.io);
-        //   });
-        // });
         var _this = this;
         this.namespace.on('connection', function (socket) {
             // we need the socket to be listening to the event here
             // there is no socket.on('this.event')
+            // socket.on('disconnect', consumer => {
+            //   consumer.
+            // });
             _this.consumerArr.forEach(function (consumer) {
                 consumer.run(_this.namespace);
             });
--- kafka-socks/Subject.ts
+++ kafka-socks/Subject.ts
@@ -4,24 +4,25 @@ import Consumer from './Consumer';
 //   on: Function;
 // }
 
-interface ConsumerInterface {
-  run: Function;
-}
+// interface ConsumerInterface {
+//   run: Function;
+//   pause: Function;
+// }
 
 class Subject {
   io: any
   namespace: any
-  consumerArr: ConsumerInterface[];
+  consumerArr: any[];
 
   // io is socket server instance
   // consumerArr is an array of KafkaSocks consumerArr (from the Consumer class we created)
-  constructor(io: any, namespace: string, consumerArr: ConsumerInterface[] = []) {
+  constructor(io: any, namespace: string, consumerArr: any[] = []) {
     this.io = io;
     this.namespace = this.io.of("/"+namespace);
     this.consumerArr = consumerArr;
   }
   // adds a KafkaSocks Consumer to the consumerArr array property
-  add(consumer: ConsumerInterface) {
+  add(consumer: any) {
     // instantiate the Kakfa Consumer using the Kafa-provided class + add that consumer to our array
     this.consumerArr.push(consumer);
   }
@@ -29,15 +30,27 @@ class Subject {
   // opening the io server
   // invoke the running of the sockets corresponding to the varoius consumerArr in our consumerArr array
   connect() {
-    this.namespace.on('connection', (socket: object) => {
+    this.namespace.on('connection', (socket: any) => {
      // we need the socket to be listening to the event here
       // there is no socket.on('this.event')
-      this.consumerArr.forEach((consumer: ConsumerInterface) => {
+
+      // socket.on('disconnect', consumer => {
+      //   consumer.
+      // });
+
+
+      this.consumerArr.forEach((consumer: any) => {
       consumer.run(this.namespace);
       });
     });
     
   }
+
+  // disconnect(event: any) {
+  //   this.namespace.on(event, (socket : object) => {
+  //     console.log('disconnected...')
+  //   })
+  // }
 }
 
 export default Subject;
