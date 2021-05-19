e23cee09ee75fc44c166c8fd1809ab47ad2f1d55
Jason Fricano <jasonfricano@Ninjas-MBP.local>
2021-05-19 14:19:54 -0400

    move existing demo to backup folder

--- demo/server/kafka.ts
+++
@@ -1,8 +0,0 @@
-import Confluent from '../../kafka-socks/Confluent';
-require('dotenv').config();
-
-const { API_KEY, API_SECRET, KAFKA_BOOTSTRAP_SERVER } = process.env;
-const broker = new Confluent(API_KEY!, API_SECRET!, KAFKA_BOOTSTRAP_SERVER!);
-const kafka = broker.create('new-client');
-
-export { kafka };
