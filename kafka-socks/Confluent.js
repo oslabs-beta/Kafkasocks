b09d070741d7f80f3d2c0893b06cffb05d24b6a2
allisonIsCoding <allison-jacobs@outlook.com>
2021-05-19 18:51:30 -0400

    added demo-2 file

--- kafka-socks/Confluent.js
+++ kafka-socks/Confluent.js
@@ -1,31 +1,30 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.Confluent = void 0;
-var Kafka = require('kafkajs').Kafka;
+const { Kafka } = require('kafkajs');
 // Confluent class instantiates the connection Confluent Kafka cluster
-var Confluent = /** @class */ (function () {
+class Confluent {
     // key is the Confluent cloud API key
     // secret is the Confluent Cloud API secret
     // server is the Confluent Cloud URI to connect to the bootstrap server
-    function Confluent(key, secret, server) {
+    constructor(key, secret, server) {
         this.key = key;
         this.secret = secret;
         this.server = server;
     }
     // create will return a Kafka Consumer object
-    Confluent.prototype.create = function (client) {
-        var sasl = this.key && this.secret
+    create(client) {
+        const sasl = this.key && this.secret
             ? { username: this.key, password: this.secret, mechanism: "plain" }
             : null;
-        var ssl = !!sasl;
+        const ssl = !!sasl;
         return new Kafka({
             clientId: client,
             brokers: [this.server],
-            ssl: ssl,
-            sasl: sasl
+            ssl,
+            sasl,
         });
-    };
-    return Confluent;
-}());
+    }
+}
 exports.Confluent = Confluent;
-exports["default"] = Confluent;
+exports.default = Confluent;
--- kafka-socks/Subject.js
+++ kafka-socks/Subject.js
@@ -1,5 +1,5 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 // interface ioInterface {
 //   on: Function;
 // }
@@ -7,35 +7,32 @@ exports.__esModule = true;
 //   run: Function;
 //   pause: Function;
 // }
-var Subject = /** @class */ (function () {
+class Subject {
     // io is socket server instance
     // consumerArr is an array of KafkaSocks consumerArr (from the Consumer class we created)
-    function Subject(io, namespace, consumerArr) {
-        if (consumerArr === void 0) { consumerArr = []; }
+    constructor(io, namespace, consumerArr = []) {
         this.io = io;
         this.namespace = this.io.of("/" + namespace);
         this.consumerArr = consumerArr;
     }
     // adds a KafkaSocks Consumer to the consumerArr array property
-    Subject.prototype.add = function (consumer) {
+    add(consumer) {
         // instantiate the Kakfa Consumer using the Kafa-provided class + add that consumer to our array
         this.consumerArr.push(consumer);
-    };
+    }
     // opening the io server
     // invoke the running of the sockets corresponding to the varoius consumerArr in our consumerArr array
-    Subject.prototype.connect = function () {
-        var _this = this;
-        this.namespace.on('connection', function (socket) {
+    connect() {
+        this.namespace.on('connection', (socket) => {
             // we need the socket to be listening to the event here
             // there is no socket.on('this.event')
             // socket.on('disconnect', consumer => {
             //   consumer.
             // });
-            _this.consumerArr.forEach(function (consumer) {
-                consumer.run(_this.namespace);
+            this.consumerArr.forEach((consumer) => {
+                consumer.run(this.namespace);
             });
         });
-    };
-    return Subject;
-}());
-exports["default"] = Subject;
+    }
+}
+exports.default = Subject;
