5617d4507ec168ab0448320cb1bc7e0f43a31f57
Jason Fricano <jasonfricano@Ninjas-MBP.local>
2021-05-19 22:02:03 -0400

    fix npm build and start scripts

--- demo-2/server/server.ts
+++ demo-2/server/server.ts
@@ -1,18 +1,30 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // app.use('/', (req: {}, res: {}) => {
 
 // })
 
 app.get('/', (req: any, res: any) => {
-  res.sendFile(__dirname + '/../client/index.html')
+  res.sendFile(path.resolve(__dirname, './../client/index.html'))
 });
 
-
+app.listen(3000, () => console.log('listening on 3000'));
 
 // 1. set up the producer to produce messages on interval
 
 // 2. create a kafkasocks instance to:
   // a. consume produced messages and 
   // b. emit them on socket
+
+// require('dotenv').config();
+// const http = require('http');
+// const express = require('express');
+// const { Server } = require('socket.io');
+
+// const app = express();
+// const server = http.createServer(app);
+// const io = new Server(server);
+
+// const PORT = 3000;
