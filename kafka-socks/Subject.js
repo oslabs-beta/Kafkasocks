6a86acb80e3b8fa1bdeca049db51216ef86af461
allisonIsCoding <allison-jacobs@outlook.com>
2021-05-22 12:01:41 -0400

    recompiled kafka-socks library

--- kafka-socks/Subject.js
+++ kafka-socks/Subject.js
@@ -1,5 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+exports.__esModule = true;
 // interface ioInterface {
 //   on: Function;
 // }
@@ -7,51 +7,45 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //   run: Function;
 //   pause: Function;
 // }
-class Subject {
-    // io is socket server instance
-    // consumerArr is an array of KafkaSocks consumerArr (from the Consumer class we created)
-    constructor(io, namespace, consumerArr = []) {
-        this.io = io;
-        this.namespace = this.io.of("/" + namespace);
-        this.consumerArr = consumerArr;
-        console.log("subject made");
+var Subject = /** @class */ (function () {
+  // io is socket server instance
+  // consumerArr is an array of KafkaSocks consumerArr (from the Consumer class we created)
+  function Subject(io, namespace, consumerArr) {
+    if (consumerArr === void 0) {
+      consumerArr = [];
     }
-    // adds a KafkaSocks Consumer to the consumerArr array property
-    add(consumer) {
-        // instantiate the Kakfa Consumer using the Kafa-provided class + add that consumer to our array
-        console.log("in Subject.add");
-        this.consumerArr.push(consumer);
-    }
-    // opening the io server
-    // invoke the running of the sockets corresponding to the varoius consumerArr in our consumerArr array
-<<<<<<< HEAD
-    connect() {
-        this.namespace.on('connection', (socket) => {
-=======
-    Subject.prototype.connect = function () {
-        var _this = this;
-        console.log("in Subject.connect()");
-        this.namespace.on("connection", function (socket) {
->>>>>>> 585729e33ad014c5446db555392d44ce435f94ed
-            // we need the socket to be listening to the event here
-            // there is no socket.on('this.event')
-            // socket.on('disconnect', consumer => {
-            //   consumer.
-            // });
-<<<<<<< HEAD
-            this.consumerArr.forEach((consumer) => {
-                consumer.run(this.namespace);
-=======
-            console.log("in namespace.on cb");
-            _this.consumerArr.forEach(function (consumer) {
-                consumer.run(_this.namespace);
-                socket.on('disconnect', function () {
-                    console.log('disconnecting');
-                    consumer.close().then(function () { return console.log('disconnected'); });
-                });
->>>>>>> 585729e33ad014c5446db555392d44ce435f94ed
-            });
-        });
-    }
-}
-exports.default = Subject;
+    this.io = io;
+    this.namespace = this.io.of('/' + namespace);
+    this.consumerArr = consumerArr;
+    console.log('subject made');
+  }
+  // adds a KafkaSocks Consumer to the consumerArr array property
+  Subject.prototype.add = function (consumer) {
+    // instantiate the Kakfa Consumer using the Kafa-provided class + add that consumer to our array
+    console.log('in Subject.add');
+    this.consumerArr.push(consumer);
+  };
+  // opening the io server
+  // invoke the running of the sockets corresponding to the varoius consumerArr in our consumerArr array
+  Subject.prototype.connect = function () {
+    var _this = this;
+    console.log('in Subject.connect()');
+    this.namespace.on('connection', function (socket) {
+      // we need the socket to be listening to the event here
+      // there is no socket.on('this.event')
+      // socket.on('disconnect', consumer => {
+      //   consumer.
+      // });
+      console.log('in namespace.on cb');
+      _this.consumerArr.forEach(function (consumer) {
+        consumer.run(_this.namespace);
+        // socket.on('disconnect', () => {
+        //   console.log('disconnecting');
+        //   consumer.disconnect().then(() => console.log('disconnected'));
+        // });
+      });
+    });
+  };
+  return Subject;
+})();
+exports['default'] = Subject;
