c5015dfc9b1fbe598c1192c176ddfd6dec9947ee
jenessachap <jenessachap@gmail.com>
2021-05-29 14:25:37 -0700

    addded custom color palette and nav bar

---
+++ playground/client/Features.tsx
@@ -0,0 +1,97 @@
+import * as React from "react";
+import { FC } from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Container,
+  makeStyles,
+  createStyles,
+} from "@material-ui/core";
+import theme from "./theme";
+import { flattenDiagnosticMessageText } from "typescript";
+
+const Features = () => {
+  const useStyles = makeStyles((theme: any) =>
+    createStyles({
+      main: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        alignContent: "center",
+      },
+      col: {
+        display: "flex",
+        flexDirection: "row",
+        justifyItems: "center",
+        alignItems: "center",
+      },
+      text: {
+        display: "flex",
+        alignContent: "center",
+      },
+      features: {
+        display: "flex",
+        alignitems: "center",
+        padding: "2rem 0",
+        width: "100%",
+      },
+      img: {
+        height: "200px",
+        width: "200px",
+      },
+    })
+  );
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.img}>
+      <CardMedia
+        component="img"
+        alt="What Matters"
+        height="200"
+        image="./Images/TeamFocus.jpg"
+        title="What Matters"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          Focus on What Matters
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Build by developers with developers in mind. Giving you freedom to //
+          make decision on how you handle streaming data.
+        </Typography>
+      </CardContent>
+    </Card>
+
+    // <Container className={classes.main}>
+    //   <div className={classes.features}>
+    //     <h3>Easy to Use</h3>
+    //     <img
+    //       src="./images/Kafkasocks-full-logo.png"
+    //       width="200px"
+    //       height="200px"
+    //     />
+    //     <p>
+    //       Kafka Socks is a simple npm install package that allows you to easily
+    //       connect your Kafka cluster to Websockets
+    //     </p>
+    //     <h3>Focus on What Matters</h3>
+    //     <p>
+    //       Build by developers with developers in mind. Giving you freedom to
+    //       make decision on how you handle streaming data.
+    //     </p>
+    //     <h3>Quick set up</h3>
+    //     <p>
+    //       After you enter your Kafka key, secret and bootstrap in the .env file,
+    //       you have the power toonnect your producer, consumer and websocket in 3
+    //       easy lines of code.
+    //     </p>
+    //   </div>
+    // </Container>
+  );
+};
+
+export default Features;
--- playground/client/theme.ts
+++ playground/client/theme.ts
@@ -1,35 +1,32 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
-  // spacing: factor => `${0.25 * factor}rem`,
   palette: {
     primary: {
-      light: '#72cff8',
-      main: '#4fc3f7',
-      dark: '#3788ac',
-      contrastText: '#fff',
+      light: "#D0E0F5",
+      main: "#BFDBF7", //<-- main fromset palette
+      dark: "#ABCFEA",
+      contrastText: "#fff",
     },
     secondary: {
-      light: '#ff7961',
-      main: '#f44336',
-      dark: '#ba000d',
-      contrastText: '#000',
+      light: "#6FABC2",
+      main: "#1F7A8C",
+      dark: "#115363",
+      contrastText: "#000",
     },
     background: {
-      default: '#f4f4f4',
+      default: "#f5f5f5",
     },
     text: {
-      primary: '#404040',
-      secondary: '#696969',
-      disabled: '#a3a3a3',
-      hint: '#a3a3a3',
+      primary: "#404040",
+      secondary: "#696969",
+      disabled: "#a3a3a3",
+      hint: "#a3a3a3",
     },
   },
   typography: {
     fontSize: 14,
-    fontFamily: [
-      'Montserrat',
-    ].join(','),
+    fontFamily: ["Montserrat"].join(","),
   },
 });
 
